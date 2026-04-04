5c33b3aad43b2ffa5797cba23cb097826d0f111d
mynameSumin <97847260+mynameSumin@users.noreply.github.com>
2026-04-04 14:48:12 +0900

    [level 2] Title: 2 x n 타일링, Time: 5.75 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    const dp = Array.from({length: n + 1}, () => 1);
+    dp[2] = 2;
+    for(let i = 3; i <= n; i++){
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+    }
+    
+    return dp[n];
+}
